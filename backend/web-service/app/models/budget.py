5bd4a72ded2b313eb7a9efe1cc924128d525608f
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-04-15 21:58:56 +0300

    add: budget model

--- backend/web-service/app/models/budget.py
+++ backend/web-service/app/models/budget.py
@@ -13,7 +13,7 @@ class BalanceSnapshot(BaseModel):
     """
     __tablename__ = 'balance_snapshot'
 
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # ID пользователя
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # ID-пользователя
     month = Column(Integer)  # Месяц снимка
     year = Column(Integer)  # Год снимка
     snapshot = Column(JSON)  # Снимок баланса в формате {user_cashbox: {balance: x, currency: y}}
@@ -25,6 +25,15 @@ class BalanceSnapshot(BaseModel):
 
     @staticmethod
     def make_balance_snapshot(user_id, month, year, snapshot, is_static):
+        """
+        Создает снимок баланса для указанного пользователя.
+        :param user_id: идентификатор пользователя
+        :param month: месяц снимка (1-12)
+        :param year: год для снимка (2025)
+        :param snapshot: словарь со значением баланса
+        :param is_static: Если True, снимок считается статичным; если False, динамическим
+        :return: cловарь с данными о снимке баланса
+        """
         new_bs = {
             'user_id': user_id,
             'month': month,
@@ -43,3 +52,33 @@ class BalanceSnapshotHistory(HistoryModel):
 
     balance_snapshot_id = Column(Integer, ForeignKey('balance_snapshot.id'), nullable=False)
     balance_snapshot = relationship('BalanceSnapshot')
+
+
+class Budget(BaseModel):
+    """
+    Модель бюджета
+    """
+    __tablename__ = 'budget'
+
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)  # ID-пользователя
+    category_id = Column(Integer, ForeignKey('category.id'), nullable=False)  # ID-категории
+    subcategory_id = Column(Integer, ForeignKey('subcategory.id'), nullable=True)  # ID-подкатегории
+    user_cashbox_id = Column(Integer, ForeignKey('user_cashbox.id'), nullable=True)  # ID-пользовательского кэш-бокса
+    month = Column(Integer)  # Месяц
+    year = Column(Integer)  # Год
+    amount = Column(Float)  # Планируемая сумма
+    currency = Column(String(5), default='RUB')  # Валюта
+    comment = Column(String(256), nullable=True)  # Комментарий пользователя
+    is_recurring = Column(Boolean, default=False)  # Повторяется ли каждый месяц
+    is_locked = Column(Boolean, default=False)  # Зафиксирован ли бюджет (нельзя редактировать после начала месяца)
+
+    # Связи
+    user = relationship("User", back_populates="user_budgets")
+
+    def __repr__(self):
+        return f"<Budget {self.month}/{self.year}, {self.user.username}>"
+
+class BudgetHistory(HistoryModel):
+    """История изменения бюджета"""
+    budget_id = Column(Integer, ForeignKey('budget.id'), nullable=False)
+    budget = relationship('Budget')
