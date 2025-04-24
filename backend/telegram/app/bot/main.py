6451895013b9d061f6505861b22a17142c253479
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-04-24 22:30:49 +0300

    add: new routers

--- backend/telegram/app/bot/main.py
+++ backend/telegram/app/bot/main.py
@@ -1,15 +1,25 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
-from app.bot.handlers import start
+from app.bot.routers import start, auth_login, auth_register, auth_link
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 TOKEN = os.getenv("TG_BOT_TOKEN")
 
+
 async def main():
+
+    if TOKEN is None:
+        raise ValueError("TG_BOT_TOKEN is not set in environment variables.")
+
     bot = Bot(token=TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
 
     dp.include_router(start.router)
+    dp.include_router(auth_login.router)
+    dp.include_router(auth_register.router)
+    dp.include_router(auth_link.router)
 
     await dp.start_polling(bot)
