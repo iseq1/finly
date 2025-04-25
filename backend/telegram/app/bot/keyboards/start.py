9fcf3e59062d27a4f915f79c84e43e7ef6f34f97
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-04-25 21:02:10 +0300

    fix: new keyboards

--- backend/telegram/app/bot/keyboards/start.py
+++ backend/telegram/app/bot/keyboards/start.py
@@ -1,15 +1,48 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-def get_start_keyboard():
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(text="🔐 Авторизация", callback_data="auth_login"),
-                InlineKeyboardButton(text="🆕 Регистрация", callback_data="auth_register"),
-            ],
-            [
-                InlineKeyboardButton(text="🔗 Привязать Telegram", callback_data="auth_link_telegram")
+class StartKeyboard:
+
+    @staticmethod
+    def get_libk_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text="🔗 Привязать Telegram", callback_data="auth_link_telegram"),
+                ]
             ]
-        ]
-    )
+        )
+
+    @staticmethod
+    def get_register_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text="🆕 Зарегистрироваться", callback_data="auth_register"),
+                ]
+            ]
+        )
+
+    @staticmethod
+    def get_login_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text="🔐 Авторизоваться", callback_data="auth_login"),
+                ]
+            ]
+        )
+
+    @staticmethod
+    def get_start_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(text="🔐 Авторизация", callback_data="auth_login"),
+                    InlineKeyboardButton(text="🆕 Регистрация", callback_data="auth_register"),
+                ],
+                [
+                    InlineKeyboardButton(text="🔗 Привязать Telegram", callback_data="auth_link_telegram")
+                ]
+            ]
+        )
