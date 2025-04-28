0454eb05abc008727ebd7ccca5106f1dbc6de10c
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-04-28 22:04:18 +0300

    add: new state for profile

--- backend/telegram/app/bot/states.py
+++ backend/telegram/app/bot/states.py
@@ -4,3 +4,8 @@ class AuthState(StatesGroup):
     authenticated = State()
     waiting_for_email = State()
     waiting_for_password = State()
+
+class EditProfileState(StatesGroup):
+    waiting_for_choosing_action = State()
+    choosing_field = State()
+    waiting_for_value = State()
