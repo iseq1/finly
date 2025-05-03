cf7f129f56a3b4365e17a194db1cd964c7e0ea34
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-03 23:00:05 +0300

    add: new states

--- backend/telegram/app/bot/states.py
+++ backend/telegram/app/bot/states.py
@@ -9,3 +9,21 @@ class EditProfileState(StatesGroup):
     waiting_for_choosing_action = State()
     choosing_field = State()
     waiting_for_value = State()
+
+class CreateNewUserCashbox(StatesGroup):
+    waiting_for_balance = State()
+    waiting_for_custom_name = State()
+    waiting_for_note = State()
+
+class TransactionState(StatesGroup):
+    waiting_for_transaction_field = State()
+    waiting_for_transaction_user_cashbox = State()
+    waiting_for_transaction_category = State()
+    waiting_for_transaction_subcategory = State()
+    waiting_for_transaction_info = State()
+    waiting_for_amount = State()
+    waiting_for_comment = State()
+    waiting_for_datetime = State()
+    waiting_for_vendor = State()
+    waiting_for_location = State()
+    waiting_for_source = State()
