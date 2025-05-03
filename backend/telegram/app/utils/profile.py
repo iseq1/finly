fd8f64b22c087f017d979fa3cd5fa4172a0ca74a
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-03 22:59:52 +0300

    add: new validating field

--- backend/telegram/app/utils/profile.py
+++ backend/telegram/app/utils/profile.py
@@ -12,9 +12,80 @@ class ValidateUserInput:
             'patronymic': self.validate_patronymic(field),
             'phone_number': self.validate_phone(field),
             'birthday': self.format_birthday(field),
+            'balance': self.validate_balannce(field),
+            'custom_name': self.validate_custom_name(field),
+            'note': self.validate_note(field),
+            'amount': self.validate_amount(field),
+            'datetime': self.validate_datetime(field),
+            'comment': self.validate_comment(field),
+            'location': self.validate_location(field),
+            'vendor': self.validate_vendor(field),
+            'source': self.validate_source(field),
         }
         return field_to_update[type]
 
+    def validate_source(self, value):
+        if not (0 <= len(value) <= 99):
+            return False, ValueError("Источник транзакции не должен быть пуст и при этом должен содержать до 99 символов.")
+        return True, value
+
+    def validate_vendor(self, value):
+        if not (0 <= len(value) <= 99):
+            return False, ValueError("Адресат транзакции не должен быть пуст и при этом должен содержать до 99 символов.")
+        return True, value
+
+    def validate_location(self, value):
+        if not (0 <= len(value) <= 99):
+            return False, ValueError("Запись местоположения совершения транзакции не должно быть пустым и при этом должно содержать до 99 символов.")
+        return True, value
+
+    def validate_comment(self, value):
+        if not (0 <= len(value) <= 99):
+            return False, ValueError("Комментарий не должен быть пуст и при этом должен содержать до 99 символов.")
+        return True, value
+
+    def validate_datetime(self, value):
+        from datetime import datetime
+        try:
+            if len(value.split()) == 1:
+                # Если указана только дата
+                date_str = value.strip()
+                datetime_obj = datetime.strptime(date_str, "%d.%m.%Y")
+                # Устанавливаем время на полдень
+                datetime_obj = datetime_obj.replace(hour=12, minute=0, second=0, microsecond=0)
+            else:
+                # Если указана дата и время
+                date_str, time_str = value.split()
+                datetime_str = f"{date_str} {time_str}"
+                datetime_obj = datetime.strptime(datetime_str, "%d.%m.%Y %H:%M")
+
+            return True, datetime_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+        except ValueError:
+            return False, ValueError(
+                "Некорректный формат даты или времени. Пожалуйста, используйте ДД.ММ.ГГГГ для даты и ЧЧ:ММ для времени.")
+
+    def validate_amount(self, value):
+        if not bool(re.match(r'^-?\d+(\.\d+)?$', value)):
+            return False, ValueError("Сумма транзакции может содержать только цифры и знак точки (199.99).")
+        return True, float(value)
+
+    def validate_note(self, value):
+        if not (0 <= len(value) <= 99):
+            return False, ValueError("Кастомное имя должно содержать до 99 символов.")
+        return True, value
+
+    def validate_custom_name(self, value):
+        if not (2 <= len(value) <= 64):
+            return False, ValueError("Кастомное имя должно содержать от 2 до 64 символов.")
+        if not re.match(r'^[A-Za-zА-Яа-яЁё\s-]+$', value):
+            return False, ValueError("Кастомное имя может содержать только буквы, пробелы и дефисы.")
+        return True, value
+
+    def validate_balannce(self, value):
+        if not bool(re.match(r'^-?\d+(\.\d+)?$', value)):
+            return False, ValueError("Баланс может содержать только цифры и знак точки (199.99).")
+        return True, float(value)
 
     def validate_username(self, value):
         if not re.match(r'^[a-zA-Z0-9]{1,64}$', value):
