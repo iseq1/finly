f24d6ec39a529b0f464eb740f20681ab137d382e
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-03 23:01:29 +0300

    add: new handler for main menu

--- backend/telegram/app/bot/handlers/menu/steps.py
+++ backend/telegram/app/bot/handlers/menu/steps.py
@@ -2,6 +2,7 @@ from aiogram.fsm.context import FSMContext
 from app.bot.handlers.base import BaseHandler
 from app.bot.keyboards.main_manu import MainMenuKeyboard
 from app.bot.keyboards.profile import ProfileKeyboard
+from app.bot.keyboards.transaction import TransactionKeyboard
 from app.utils.logger import logger
 
 
@@ -25,3 +26,15 @@ class SendProfileHandler(BaseHandler):
             text='Меню профиля:',
             reply_markup=ProfileKeyboard.get_profile_menu_keyboard()
         )
+        return await super().handle(event, state, context)
+
+
+class SendTransactionHandler(BaseHandler):
+    async def handle(self, event, state: FSMContext, context: dict = None):
+        logger.debug(f"[{self.__class__.__name__}] Отправка сообщения с меню транзакций авторизированному пользователю {event.from_user.id}")
+
+        await event.message.edit_text(
+            text='Меню транзакций:',
+            reply_markup=TransactionKeyboard.get_transaction_menu_keyboard()
+        )
+        return await super().handle(event, state, context)
