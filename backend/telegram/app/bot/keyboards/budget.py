34095f4b118285bc4ddefcdda4c606b4cba0941a
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-04 21:25:01 +0300

    add: new button and keyboard

--- backend/telegram/app/bot/keyboards/budget.py
+++ backend/telegram/app/bot/keyboards/budget.py
@@ -7,6 +7,10 @@ class BudgetKeyboard:
         return InlineKeyboardButton(text='Бюджеты', callback_data='budget_menu')
 
     @staticmethod
+    def get_back_to_budget_menu_button():
+        return InlineKeyboardButton(text='Вернуться в меню бюджетов', callback_data='budget_menu')
+
+    @staticmethod
     def get_back_budget_menu_button():
         return InlineKeyboardButton(text='Вернуться в меню бюджетов', callback_data='budget_menu')
 
@@ -22,7 +26,7 @@ class BudgetKeyboard:
 
     @staticmethod
     def get_my_budgets_button():
-        return InlineKeyboardButton(text='Мои бюджеты', callback_data='my_budget_menu')
+        return InlineKeyboardButton(text='Мои бюджеты', callback_data='get_my_budget_menu')
 
     @staticmethod
     def get_create_new_budget_button():
@@ -30,7 +34,7 @@ class BudgetKeyboard:
 
     @staticmethod
     def get_balance_snapshot_button():
-        return InlineKeyboardButton(text='Состояние баланса', callback_data='my_balance_snapshot_menu')
+        return InlineKeyboardButton(text='Состояние баланса', callback_data='get_my_balance_snapshot_menu')
 
     @staticmethod
     def get_budget_menu_keyboard():
@@ -51,3 +55,47 @@ class BudgetKeyboard:
                 ]
             ]
         )
+
+    @staticmethod
+    def get_empty_budgets_menu_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    BudgetKeyboard.get_create_new_budget_button()
+                ],
+                [
+                    BudgetKeyboard.get_back_to_budget_menu_button()
+                ]
+            ]
+        )
+
+    @staticmethod
+    def get_select_user_budget_button():
+        return InlineKeyboardButton(text='Выбрать', callback_data='select_user_budget')
+
+    @staticmethod
+    def get_previous_user_budget_button():
+        return InlineKeyboardButton(text='◀️ Предыдущий', callback_data="user_budget_prev")
+
+    @staticmethod
+    def get_next_user_budget_button():
+        return InlineKeyboardButton(text='Следующий ▶️', callback_data="user_budget_ext")
+
+    @staticmethod
+    def get_user_budgets_menu_keyboard():
+        return InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    BudgetKeyboard.get_previous_user_budget_button(),
+                    BudgetKeyboard.get_select_user_budget_button(),
+                    BudgetKeyboard.get_next_user_budget_button()
+                ],
+                [
+                    BudgetKeyboard.get_back_to_budget_menu_button()
+                ]
+            ]
+        )
+
+
+
+
