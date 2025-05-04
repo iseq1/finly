dde1f132b9dc1d45cc0674e71ec2def20efa61ff
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-04 21:24:31 +0300

    add: get user budgets chain (pre-done)

---
+++ backend/telegram/app/bot/handlers/menu/budget/chain.py
@@ -0,0 +1,31 @@
+from app.bot.handlers.menu.budget.steps import GetUserBudgetInfoHandler, CheckUserBudgetsInfoHandler, \
+    GetCategoryInfoForBudgetsHandler, GetSubcategoryInfoForBudgetsHandler, GetUserCashboxInfoForBudgetsHandler, \
+    CheckUserBudgetsAdditionalInfoHandler, ShowUserBudgetsHandler
+
+
+class BudgetMenuChain:
+
+    def get_handler_by_step(self, step: int, chain_func):
+        current = chain_func()
+        for _ in range(step):
+            if current.next_handler is None:
+                return None
+            current = current.next_handler
+        return current
+
+    def get_user_budgets_chain(self):
+        return GetUserBudgetInfoHandler(
+            CheckUserBudgetsInfoHandler(
+                GetCategoryInfoForBudgetsHandler(
+                    GetSubcategoryInfoForBudgetsHandler(
+                        GetUserCashboxInfoForBudgetsHandler(
+                            CheckUserBudgetsAdditionalInfoHandler(
+                                ShowUserBudgetsHandler(
+
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
