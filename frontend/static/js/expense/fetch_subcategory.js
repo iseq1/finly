2acdad7f2c3f772735a8b22b81efc77ae163ef7b
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-27 22:38:07 +0300

    add: subcategory GET request for expense

---
+++ frontend/static/js/expense/fetch_subcategory.js
@@ -0,0 +1,20 @@
+import { getAccessToken, logout } from '/static/js/security/token.js';
+const API_URL = 'http://localhost:7020/api';
+
+export async function fetchExpenseSubcategories(category_id) {
+  const token = await getAccessToken();
+  if (!token) return [];
+
+  const res = await fetch(`${API_URL}/settings/subcategories?category_id=${category_id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!res.ok) {
+    if (res.status === 401) logout();
+    return [];
+  }
+
+  return await res.json();
+}
