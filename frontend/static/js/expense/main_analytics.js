e7cf6bdddacc9fac6fa87ab835543f6261e01aaf
Yegor Mironov <EPMironov@stud.kpfu.ru>
2025-05-29 23:49:19 +0300

    add: main script for expense analytics

---
+++ frontend/static/js/expense/main_analytics.js
@@ -0,0 +1,66 @@
+import { renderExpenseStatisticsTable } from '/static/js/expense/render_transaction_statistic_table.js';
+import { handleCategoryPopup } from '/static/js/expense/handle_popup_by_category.js';
+import { handleProviderPopup } from '/static/js/expense/handle_popup_by_cashbox.js';
+import { handleDetailPopup } from '/static/js/expense/handle_popup_detail.js';
+import { getDateRangeFromInputs } from '/static/js/expense/utils.js';
+import { hidePopup } from '/static/js/expense/show_provider_popup.js';
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const now = new Date();
+  const start = new Date(now.getFullYear(), now.getMonth(), 1);
+  const end = new Date();
+  const start_date = start.toLocaleDateString('en-CA').split('T')[0];
+  const end_date = end.toLocaleDateString('en-CA').split('T')[0];
+  await renderExpenseStatisticsTable({ start_date, end_date });
+
+});
+
+document.querySelector('.popup-close')?.addEventListener('click', hidePopup);
+
+document.addEventListener('click', async (event) => {
+  const providerTh = event.target.closest('th[data-provider-id]');
+  const categoryTh = event.target.closest('th[data-category-id]');
+  const cell = event.target.closest('td[data-key]');
+
+  if (providerTh) {
+      handleProviderPopup(providerTh.dataset.providerId);
+      return;
+  }
+  if (categoryTh) {
+    handleCategoryPopup(categoryTh.dataset.categoryId);
+    return;
+  }
+  if (cell) {
+    const key = cell.dataset.key;
+    const [providerId, categoryId] = key.split('__');
+    handleDetailPopup(categoryId, providerId)
+    return;
+  }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const filterButton = document.querySelector('.actions input[type="submit"]');
+  if (!filterButton) return;
+
+  filterButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    const {start_date, end_date, include_empty_categories} = getDateRangeFromInputs();
+    if (!{start_date, end_date, include_empty_categories}) return;
+
+    // 3. Вызываем функцию рендера
+    await renderExpenseStatisticsTable({
+      start_date,
+      end_date,
+      include_empty_categories
+    });
+
+  });
+});
+
+
+
+
+
+
